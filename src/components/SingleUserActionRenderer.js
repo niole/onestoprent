5fd25400ee3c5580d5d4869d3e159b9b73335caf
niole <niolenelson@gmail.com>
2017-09-30 15:27:04 -0700

    Get basic contract sign deadline

--- src/components/SingleUserActionRenderer.js
+++ src/components/SingleUserActionRenderer.js
@@ -62,9 +62,64 @@ const SingleUserActionRenderer = Vue.component('single-user-action-renderer', {
       `
     },
     contractsign: {
+      props: [
+        'renterData',
+        'landlordUserId',
+        'renterUserId',
+        'currentUserIsRenter',
+        'contract',
+        'actionLevel',
+      ],
+      computed: {
+        mainMessage: function() {
+          if (this.currentUserIsRenter) {
+            // is renter
+            switch(this.actionLevel) {
+              case "ontime":
+                return "You contract sign deadline is today.";
+              case "approaching":
+                return `Your contract sign deadline is ${contract.signDeadline}.`;
+              case "late":
+                return `You missed your contract sign deadline on ${contract.signDeadline}. Contact your landlord if you'd like to start over.`;
+              default:
+                console.warn("This user action doesn't exist");
+                return "";
+            }
+          } else {
+            // is landlord
+            switch(this.actionLevel) {
+              case "ontime":
+                return `${this.renterData.name} signed their contract.`;
+              case "approaching":
+                return `${this.renterData.name}'s contract sign deadline is approaching.`;
+              case "late":
+                return `${this.renterData.name}'s contract sign deadline has expired.`;
+              default:
+                console.warn("This user action doesn't exist");
+                return "";
+            }
+          }
+        },
+        lesseeName: function() {
+          if (this.currentUserIsRenter) {
+            return "Your";
+          }
+          return `${this.renterData.name}'s`;
+        }
+      },
+
       template: `
         <div>
-          SIGN CONTRACT
+          <h1>
+            Sign Contract
+          </h1>
+          {{ mainMessage }}
+          <highlevel-contract
+            :contract="contract"
+            :lesseeName="lesseeName"
+            :active="false"
+            :signatureDeadline="false"
+          />
         </div>
       `
     },
